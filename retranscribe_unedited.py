0a4cb3ac8a1bdb94c013cfa9304e6d70f5fa9e57
Eliran Abdoo <eliran@riverside.fm>
2024-04-28 16:51:56 +0300

    state working

--- retranscribe_unedited.py
+++ retranscribe_unedited.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from datetime import datetime, timedelta
+from datetime import datetime
 import pickle
 import time
 from typing import List, Optional
@@ -23,6 +23,7 @@ class GetRelevantSessionsConfig(BaseSettings):
     sf_database: str = "SEGMENT_EVENTS"
     sf_schema: str = "WEB"
     start_date: datetime = Field(default_factory=lambda: datetime.now())
+    already_completed_sessions: List[str] = Field(default_factory=list)
 
 
 class FilterEditedSessionsConfig(BaseSettings):
@@ -65,6 +66,8 @@ def _fetch_data_by_session_id(connection, table_name, session_ids):
     """
     Fetch data from PostgreSQL table based on session_id.
     """
+    if len(session_ids) == 0:
+        return []
     query = f"""SELECT distinct(session_id) 
     FROM {table_name} 
     WHERE session_id in ({','.join(map(lambda s: f"'{s}'", session_ids))}) and (edited_words_v2 is not NULL) group by session_id"""
@@ -84,9 +87,14 @@ def get_relevant_sessions(config: GetRelevantSessionsConfig) -> List[str]:
         "schema": config.sf_schema,
     }
     session = Session.builder.configs(connection_parameters).create()
-    query = _get_sf_query(config.start_date)
-    sf_rows = session.sql(query).collect()
-    session_ids = [row["SESSION_ID"] for row in sf_rows]
+    sf_query_template = _get_sf_query(config.start_date)
+    sf_rows = session.sql(sf_query_template).collect()
+    session_ids = [
+        row["SESSION_ID"]
+        for row in sf_rows
+        if row["SESSION_ID"] not in set(config.already_completed_sessions)
+    ]
+
     return session_ids
 
 
@@ -125,25 +133,31 @@ def run_retranscribe(session_ids: List[str], config: RetranscribeConfig) -> None
 
 
 @dataclass
+class State:
+    already_completed_sessions: List[str]
+    last_sealed_date: datetime
+
+
+@dataclass
 class StateHandler:
     state_pkl_path: str
 
-    def load(self) -> Optional[datetime]:
+    def load(self) -> Optional[State]:
         try:
             with open(self.state_pkl_path, "rb") as f:
                 return pickle.load(f)
         except FileNotFoundError:
             return None
 
-    def save(self, state: datetime) -> None:
+    def save(self, state: State) -> None:
         with open(self.state_pkl_path, "wb") as f:
             pickle.dump(state, f)
 
 
 class StateConfig(BaseSettings):
-    state_pkl_path: str = "last_finish_date.pkl"
-    load_state: bool = False
-    save_state: bool = False
+    state_pkl_path: str = "state.pkl"
+    load_state: bool = True
+    save_state: bool = True
 
 
 def main() -> None:
@@ -152,14 +166,22 @@ def main() -> None:
     state_handler = StateHandler(state_pkl_path=state_config.state_pkl_path)
     get_relevant_sessions_config = GetRelevantSessionsConfig()
     if state_config.load_state:
-        start_date = state_handler.load()
-        if start_date is not None:
-            get_relevant_sessions_config.start_date = start_date
-    sessions = get_relevant_sessions(GetRelevantSessionsConfig())
+        state = state_handler.load()
+        if state is not None:
+            get_relevant_sessions_config.already_completed_sessions = (
+                state.already_completed_sessions
+            )
+            get_relevant_sessions_config.start_date = state.last_sealed_date
+    sessions = get_relevant_sessions(get_relevant_sessions_config)
     edited_sessions = get_unedited_sessions(sessions, FilterEditedSessionsConfig())
     run_retranscribe(edited_sessions, RetranscribeConfig())
     if state_config.save_state:
-        state_handler.save(now)
+        state = State(
+            already_completed_sessions=get_relevant_sessions_config.already_completed_sessions
+            + edited_sessions,
+            last_sealed_date=now,
+        )
+        state_handler.save(state)
 
 
 def _get_sf_query(date: datetime):
